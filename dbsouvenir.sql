64018bdb06526d7f6164b9621506a96b79ccc1ac
egodasa <egodasa@gmail.com>
2018-04-18 10:34:05 +0700

    Penyesuaian database

--- dbsouvenir.sql
+++ dbsouvenir.sql
@@ -5,72 +5,19 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-DROP TABLE IF EXISTS `tbl_detail_pesanan`;
-CREATE TABLE `tbl_detail_pesanan` (
-  `nama_mempelai` text NOT NULL,
-  `nama_orangtua` text NOT NULL,
-  `tgl_akadnikah` date NOT NULL,
-  `tgl_resepsi` date NOT NULL,
-  `waktu_akadnikah` time NOT NULL,
-  `waktu_resepsi` time NOT NULL,
-  `alamat_akadnikah` varchar(255) NOT NULL,
-  `alamat_resepsi` varchar(255) NOT NULL,
-  `anggota_keluarga` text NOT NULL,
-  `foto_lokasi` text NOT NULL,
-  `id_detail` int(11) NOT NULL AUTO_INCREMENT,
-  `id_pemesanan` int(11) NOT NULL,
-  PRIMARY KEY (`id_detail`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `tbl_kota`;
+CREATE TABLE `tbl_kota` (
+  `id_kota` int(11) NOT NULL AUTO_INCREMENT,
+  `nm_kota` varchar(50) NOT NULL,
+  `tarif` int(11) NOT NULL,
+  PRIMARY KEY (`id_kota`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
-
-DROP TABLE IF EXISTS `tbl_foto_produk`;
-CREATE TABLE `tbl_foto_produk` (
-  `id_foto_produk` int(11) NOT NULL AUTO_INCREMENT,
-  `id_produk` int(11) NOT NULL,
-  `foto_produk` text NOT NULL,
-  PRIMARY KEY (`id_foto_produk`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
-
-
-DROP TABLE IF EXISTS `tbl_pembayaran`;
-CREATE TABLE `tbl_pembayaran` (
-  `id_pembayaran` int(11) NOT NULL AUTO_INCREMENT,
-  `id_pemesanan` varchar(50) NOT NULL,
-  `nama_pembayar` varchar(100) NOT NULL,
-  `no_rek` varchar(30) NOT NULL,
-  `nama_bank` varchar(50) NOT NULL,
-  `tgl_pembayaran` date NOT NULL,
-  `total` int(11) NOT NULL,
-  `foto_bukti` text NOT NULL,
-  `status_pembayaran` enum('Diproses','Diterima','Ditolak') NOT NULL DEFAULT 'Diproses',
-  PRIMARY KEY (`id_pembayaran`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
-
-DROP TABLE IF EXISTS `tbl_pemesanan`;
-CREATE TABLE `tbl_pemesanan` (
-  `id_pemesanan` varchar(100) NOT NULL,
-  `nama_pemesan` varchar(200) NOT NULL,
-  `alamat_pemesan` varchar(255) NOT NULL,
-  `no_telp` varchar(14) NOT NULL,
-  `id_user` int(11) NOT NULL,
-  `total_harga` int(11) NOT NULL,
-  `jumlah_pesan` int(11) NOT NULL,
-  `id_produk` int(11) NOT NULL,
-  `tgl_pesan` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id_pemesanan`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
-DROP TABLE IF EXISTS `tbl_produk`;
-CREATE TABLE `tbl_produk` (
-  `id_produk` int(11) NOT NULL AUTO_INCREMENT,
-  `nm_produk` varchar(20) NOT NULL,
-  `jenis_produk` varchar(15) NOT NULL,
-  `harga` int(11) NOT NULL,
-  PRIMARY KEY (`id_produk`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
-
+TRUNCATE `tbl_kota`;
+INSERT INTO `tbl_kota` (`id_kota`, `nm_kota`, `tarif`) VALUES
+(1,	'Padang',	50000),
+(2,	'Dhamasraya',	150000),
+(3,	'Pasaman',	130000);
 
 DROP TABLE IF EXISTS `tbl_user`;
 CREATE TABLE `tbl_user` (
@@ -80,16 +27,13 @@ CREATE TABLE `tbl_user` (
   `email` varchar(20) NOT NULL,
   `tipe_user` varchar(15) NOT NULL,
   PRIMARY KEY (`id_user`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
 TRUNCATE `tbl_user`;
 INSERT INTO `tbl_user` (`id_user`, `username`, `password`, `email`, `tipe_user`) VALUES
 (7,	'ayam',	'e070e2dd9634c6c078a59218cdca9e23',	'egodasa@gmail.com',	'Admin'),
 (8,	'pembeli',	'a9f8bbb8cb84375f241ce3b9da6219a1',	'pem@fsd.com',	'Pelanggan'),
 (9,	'admin',	'21232f297a57a5a743894a0e4a801fc3',	'admin@adminc.com',	'Admin'),
-(10,	'egodasa',	'e070e2dd9634c6c078a59218cdca9e23',	'egodasa@gmail.com',	'Pelanggan'),
-(12,	'asda',	'e070e2dd9634c6c078a59218cdca9e23',	'egodasa@gmail.com',	'Pelanggan'),
-(13,	'egodasa',	'e070e2dd9634c6c078a59218cdca9e23',	'egodasa@gmail.com',	'Pelanggan'),
-(14,	'egodasa',	'e070e2dd9634c6c078a59218cdca9e23',	'egodasa@gmail.com',	'Pelanggan');
+(10,	'egodasa',	'e070e2dd9634c6c078a59218cdca9e23',	'egodasa@gmail.com',	'Pelanggan');
 
--- 2018-04-14 14:20:40
+-- 2018-04-18 03:32:50
