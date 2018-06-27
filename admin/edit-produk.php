ed433b97b5a0335effd4a2f69709b20f50f88152
Ego Dafma Dasa <egodasa@gmail.com>
2018-06-27 17:55:45 +0700

    Perbaikan edit produk dan penghapusan tombol cetak faktur transaksi

--- admin/edit-produk.php
+++ admin/edit-produk.php
@@ -5,15 +5,7 @@ require "../koneksi.php";
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
 	$d = $_POST;
 	$db->from('tbl_produk')->where('id_produk', $_GET['id_produk'])->update(array('nm_produk' => $d['nm_produk'],'harga' => $d['harga']))->execute();
-    $x = 0;
-    foreach($_FILES['foto_produk']['name'] as $f){
-		$path = "../produk/";
-		$path = $path.basename($f);
-		move_uploaded_file($_FILES['foto_produk']['tmp_name'][$x], $path);
-		$db->from('tbl_foto_produk')->where("id_foto_produk", $d['id_foto_produk'][$x])->update(array("foto_produk" => $id, "foto_produk"	=> $f))->execute();
-		$x++;
-	}
-	header('Location: '.$base_url.'/admin/kelola-produk.php');
+    header('Location: '.$base_url.'/admin/kelola-produk.php');
 }else{
 	if(!isset($_GET['id_produk'])){
 		header('Location: '.$base_url);
@@ -60,12 +52,20 @@ foreach($detailProduk as $p):
 <div class="col-sm-4">
 <div class="form-group">
     <form method="POST" enctype="multipart/form-data" action="<?=$base_url;?>/admin/edit-foto-produk.php">
-    <img src="<?=$base_url;?>/produk/<?=$p['foto_produk'];?>" width="200" height="200" />
-	<input name="foto_produk" type="file" class="form-control">
-	<input name="id_foto_produk" type="hidden" class="form-control" value="<?=$p['id_foto_produk']?>">
-	<input name="id_produk" type="hidden" class="form-control" value="<?=$_GET['id_produk']?>">
+    <?php if($p['foto_produk'] != ""): ?>
+        <img src="<?=$base_url;?>/produk/<?=$p['foto_produk'];?>" width="200" height="200" />
+        <input name="foto_produk" type="file" class="form-control">
+    <?php else: ?>
+        <input style="margin-top:200px;" name="foto_produk" type="file" class="form-control">
+    <?php endif; ?>
+    <input name="id_foto_produk" type="hidden" class="form-control" value="<?=$p['id_foto_produk']?>">
+    <input name="id_produk" type="hidden" class="form-control" value="<?=$_GET['id_produk']?>">
     <br/>
-    <button type="submit"class="btn btn-primary">Edit Foto</button>
+    <?php if($p['foto_produk'] == ""): ?>
+        <button type="submit" class="btn btn-success">Tambah Foto</button>
+    <?php else: ?>
+        <button type="submit" class="btn btn-primary">Edit Foto</button>
+    <?php endif; ?>
     </form>
 </div>
 </div>
