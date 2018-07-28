dbd2c0f6d34e7347373658405fa71992ca0648bd
Ego Dafma Dasa <egodasa@gmail.com>
2018-07-28 21:51:57 +0700

    Revisi database

--- dbsouvenir.sql
+++ dbsouvenir.sql
@@ -1,3 +1,10 @@
+-- Adminer 4.6.2 MySQL dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
 DROP TABLE IF EXISTS `tbl_detail_pesanan`;
 CREATE TABLE `tbl_detail_pesanan` (
   `nama_mempelai` text NOT NULL,
@@ -11,7 +18,7 @@ CREATE TABLE `tbl_detail_pesanan` (
   `anggota_keluarga` text NOT NULL,
   `foto_lokasi` text NOT NULL,
   `id_detail` int(11) NOT NULL AUTO_INCREMENT,
-  `id_pemesanan` varchar(100) NOT NULL,
+  `id_pemesanan` varchar(50) NOT NULL,
   PRIMARY KEY (`id_detail`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -24,6 +31,7 @@ CREATE TABLE `tbl_foto_produk` (
   PRIMARY KEY (`id_foto_produk`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+
 DROP TABLE IF EXISTS `tbl_kota`;
 CREATE TABLE `tbl_kota` (
   `id_kota` int(11) NOT NULL AUTO_INCREMENT,
@@ -32,10 +40,18 @@ CREATE TABLE `tbl_kota` (
   PRIMARY KEY (`id_kota`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `tbl_kota` (`id_kota`, `nm_kota`, `tarif`) VALUES
-(1,	'Padang Panjang',	30000),
-(2,	'Painan',	50000)
-ON DUPLICATE KEY UPDATE `id_kota` = VALUES(`id_kota`), `nm_kota` = VALUES(`nm_kota`), `tarif` = VALUES(`tarif`);
+
+DROP TABLE IF EXISTS `tbl_pelanggan`;
+CREATE TABLE `tbl_pelanggan` (
+  `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) NOT NULL,
+  `nm_pelanggan` varchar(100) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `no_telpon` varchar(14) NOT NULL,
+  `jk` varchar(15) NOT NULL,
+  PRIMARY KEY (`id_pelanggan`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 DROP TABLE IF EXISTS `tbl_pembayaran`;
 CREATE TABLE `tbl_pembayaran` (
@@ -51,6 +67,7 @@ CREATE TABLE `tbl_pembayaran` (
   PRIMARY KEY (`id_pembayaran`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+
 DROP TABLE IF EXISTS `tbl_pemesanan`;
 CREATE TABLE `tbl_pemesanan` (
   `id_pemesanan` varchar(100) NOT NULL,
@@ -65,6 +82,7 @@ CREATE TABLE `tbl_pemesanan` (
   `id_kota` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_pemesanan`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 
 DELIMITER ;;
 
@@ -83,6 +101,7 @@ CREATE TABLE `tbl_produk` (
   PRIMARY KEY (`id_produk`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+
 DROP TABLE IF EXISTS `tbl_user`;
 CREATE TABLE `tbl_user` (
   `id_user` int(11) NOT NULL AUTO_INCREMENT,
@@ -93,13 +112,5 @@ CREATE TABLE `tbl_user` (
   PRIMARY KEY (`id_user`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `tbl_user` (`id_user`, `username`, `password`, `email`, `tipe_user`) VALUES
-(8,	'pembeli',	'a9f8bbb8cb84375f241ce3b9da6219a1',	'pem@fsd.com',	'Pelanggan'),
-(9,	'admin',	'21232f297a57a5a743894a0e4a801fc3',	'admin@adminc.com',	'Admin'),
-(10,	'egodasa',	'e070e2dd9634c6c078a59218cdca9e23',	'egodasa@gmail.com',	'Pelanggan'),
-(12,	'asda',	'e070e2dd9634c6c078a59218cdca9e23',	'egodasa@gmail.com',	'Pelanggan'),
-(13,	'egodasa',	'e070e2dd9634c6c078a59218cdca9e23',	'egodasa@gmail.com',	'Pelanggan'),
-(14,	'egodasa',	'e070e2dd9634c6c078a59218cdca9e23',	'egodasa@gmail.com',	'Pelanggan'),
-(15,	'pengguna',	'200820e3227815ed1756a6b531e7e0d2',	'pengguna@email.com',	'Pelanggan')
-ON DUPLICATE KEY UPDATE `id_user` = VALUES(`id_user`), `username` = VALUES(`username`), `password` = VALUES(`password`), `email` = VALUES(`email`), `tipe_user` = VALUES(`tipe_user`);
 
+-- 2018-07-28 14:51:33
